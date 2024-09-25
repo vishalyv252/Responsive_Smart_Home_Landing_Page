ba49ef1f0e3864b54fbdcfe425404b78f9b0dc40
vishalyv252 <vishalyella@gmail.com>
2024-09-25 15:22:39 +0530

    Finished Project

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: '**', component: NotFoundPageComponent }
+]
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [RouterModule],
   template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
+    <router-outlet></router-outlet>
   `,
 })
 export class App {
-  name = 'Angular';
 }
 
-bootstrapApplication(App);
+bootstrapApplication(App,{
+  providers:[provideRouter(routes)]
+});
